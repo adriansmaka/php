c8dd88fbf19b89f813e9379ec6a3e23757a94e78
ElektriikAtk <adrian.smaka@edu.nykoping.se>
2021-10-05 09:28:32 +0200

    Kapitel 2 klart

---
+++ uppgift10.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <p>Mät in antal inmatade pengar <br><input></p> -->
+    <?php
+    $in = "5123.64";
+    $mod = $in % 100;
+    $ut = $in - $mod;
+    /* $ut = (int)$ut; */
+    $antal = $ut / 100;
+    echo "<p>Uttagsautomat - Hundralappar</p> <p>Inmatat: $in, uttag: $ut ($antal hundralappar)</p>";
+    ?>
+</body>
+</html>
