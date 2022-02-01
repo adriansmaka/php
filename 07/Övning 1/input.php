1a81c28b5418fc364916bd1d03b7649da9f1979d
ElektriikAtk <adrian.smaka@edu.nykoping.se>
2022-02-01 09:54:18 +0100

    Pushat

--- 07/Övning 1/input.php
+++ 07/Övning 1/input.php
@@ -1,4 +1,7 @@
 <?php
+
+    setlocale(LC_TIME, "sv_SE");
+
     $file = "data.txt";
     $text = "This is a test.";
     $ftext = filter_var($text, FILTER_SANITIZE_STRING);
@@ -18,4 +21,36 @@
         echo "<p>The content of the file is the following:</p>";
         print_r($f);
     }
+
+$today = getdate();
+foreach ($today as $key => $value) {
+    if ($key == "mday" or $key == "month" or $key == "year") {
+        if ($value <= 31 and (is_numeric($value) == True)) {
+            $mday = $value;
+        } if ($value > 31 and (is_numeric($value) == True)) {
+            $year = $value;
+        } if (is_string($value) == True) { //Fixa det här, hämta månad från tal till sträng!
+            $mon = $value;
+        }
+    }
+}
+
+echo "<p>$mday $mon $year</p>";
+
+$m_numtostr = array(
+    1 => "Januari",
+    "Februari",
+    "Mars",
+    "April",
+    "Maj",
+    "Juni",
+    "Juli",
+    "Augusti",
+    "September",
+    "Oktober",
+    "November",
+    "December"
+    )
+
+
 ?>
